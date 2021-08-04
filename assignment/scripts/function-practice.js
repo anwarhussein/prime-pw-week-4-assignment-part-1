d85a30a44dea16edb3f841ace700b3578d9bf2be
Anwar Hussein <anwarhussein2353@gmail.com>
2021-08-04 11:55:51 -0500

    Week 4 part 1 first commit

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,75 +15,99 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return name;
 }
 // Remember to call the function to test
+console.log(`Hello, ${helloName('Stacy!')}`);
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber ) {
+  return firstNumber + secondNumber;
 }
-
+console.log(`Adding 5 and 9: ${addNumbers(5,9)}`);
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(first,second,third){
+  return first * second * third;
 }
+console.log(`Multiplying 3 by 6 by 9 yields: ${multiplyThree(3,6,9)}`);
 
-
-// 5. Function that will return true if a number is positive, 
+// 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+    return false;
 }
-// Call the function to test each outcome (true & false) 
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  return array.pop();
 }
+console.log(`Returning the last element in the array: ${getLast([2,5,9,10,999,20,15])}`);
+console.log(`Returning the last element in an empty array: ${getLast([])}`);
 
-// 7. Function to find a value in an array. Return true if the 
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-  
+ for(let element of array){
+   if(value === element)
+     return true;
+ }
+ return false;
 }
+let numArray = [1, 9, 16, 12, 100];
+const result1 = find( 9, numArray );
+const result2 = find ( 6, numArray);
+console.log(result1);
+console.log(result2);
 
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  for(let character = 0; character < string.length; character++){
+    return letter === string[0];
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+function sumAll(array ) {
+  let sum = 0;
   // TODO: loop to add items
+  for(let number of array){
+    sum += number;
+  }
   return sum;
 }
-
+console.log(`Returning sum of the array: ${sumAll([1, 9, 16, 12, 100])}`)
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
-
-
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+function newPossArray(array){
+  let newArr = [];
+  for (let num of array) {
+    if (num > 0)
+      newArr.push(num);
+  }
+    return newArr;
+}
+console.log(newPossArray([1, 9, 16, 12, 100]))
+console.log(newPossArray([-1, -9, -16, -12, 100]))
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
